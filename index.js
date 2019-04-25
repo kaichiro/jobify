127824f3bdcd27f3a40e1073ef8385c3f6b2b8f4
kaichiro <kcfg23@gmail.com>
2019-04-24 22:14:14 -0300

    Setando public dir

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // region Routes
