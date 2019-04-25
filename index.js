eb9393ca6a617c1c09cebf0fac0c19e783bf04d9
kaichiro <kcfg23@gmail.com>
2019-04-24 22:10:03 -0300

    Setando caminho das views

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 // endregion
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
